919bf1c1bbfaa056244300748d5b7f9340db5297
SHLee84 <sanghyun.lee@shlee.ca>
2016-01-31 22:01:42 -0500

    side menu test
    nothing major

--- content/includes/side_menu.php
+++ content/includes/side_menu.php
@@ -1,3 +1,7 @@
+	<?php 
+	session_start();
+	//$_SESSION["username"] = "Coreators";
+	?>
 	<nav>
 		<ul class="side_menu">
 			<li><a class="scroll" href="#about">About</a></li>
@@ -5,7 +9,14 @@
 			<li><a class="scroll" href="#contact">Contact</a></li>
 		</ul>
 		<hr />
+	<?php if (isset($_SESSION["username"]) && $_SESSION["username"] != "") { ?>
+		<ul class="side_menu">
+			<li><a><small>Logged in as Coreators</small></a></li>
+		</ul>
+		<?php } else { ?>
 		<ul class="side_menu login">
 			<li><a class="scroll" data-toggle="modal" data-target="#loginModal">Log In</a></li>
 		</ul>
+		<?php } ?>
+	<?php session_destroy(); ?>
 	</nav>
