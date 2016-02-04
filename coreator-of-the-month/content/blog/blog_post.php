3bce60070d142c1d34acfe398fa895e97743e23c
SHLee84 <sanghyun.lee@shlee.ca>
2016-02-03 22:21:36 -0500

    added korean
    added korean version of COTM desc

--- coreator-of-the-month/content/blog/blog_post.php
+++ coreator-of-the-month/content/blog/blog_post.php
@@ -14,9 +14,11 @@
 								<p><br /><br /><?php echo $post["content"]; ?></p>
 								<hr width="100%" />
 								<p>
+									이 달의 코리에이터’는 월간 인터뷰 시리즈입니다. 한인 예술인 커뮤니티를 구축하는 데에서 이미 각자의 분야에서 여러 가지를 성취하신 기성 예술인분들의 이야기를 공유하는 것을 목표로 하며 예술에 관심이 있는 청년층에게 긍정적인 영향을 줄 수 있는 프로젝트로 자리매김하고 있습니다.<br /><br />
 									The Coreator of the Month series is one of our marketing projects where we interview established Korean-Canadian artists in their respective fields and write an article (both video and text included) to capture their story as an inspiration for the younger generation of Korean-Canadian community that is interested in arts or any creative pursuits.
 								</p>
 								<p>
+									인터뷰 담당자: <i>전지원, 방서원, 도세진</i><br />
 									Interviewer: <i>Jiwon Chun, Seowon Bang, Sejin Do</i>
 								</p>
 							</div>
